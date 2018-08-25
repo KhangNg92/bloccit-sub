781c34a2a160839b30e2b173dc65bc499e26cada
khang nguyen <mkavo92@gmail.com>
2018-08-25 02:27:18 -0500

    Authorization checkpoint completed

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -5,7 +5,7 @@ const base = "http://localhost:3000/topics/";
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const User = require("../../src/db/models").User;
-// admin sections failed
+// admin sections work
 describe("routes : topics", () => {
 
   beforeEach((done) => {
