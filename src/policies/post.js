2cbd02eca04e54575f00e7670b00e4bdf7acc724
khang nguyen <mkavo92@gmail.com>
2018-08-26 21:17:32 -0500

    Authorization assignment completed

--- src/policies/post.js
+++ src/policies/post.js
@@ -1,24 +1,31 @@
-const ApplicationPolicy = require("./application");
+ // #1
+ const ApplicationPolicy = require("./application");
 
-module.exports = class PostPolicy extends ApplicationPolicy {
+ module.exports = class PostPolicy extends ApplicationPolicy {
+ 
+   new() {
+     return this.user != null || this._isAdmin();
+   }
+ 
+   create() {
+     return this.user != null || this._isAdmin();
+   }
+ 
+   show() {
+     return true;
+   }
+ 
+ 
+   edit() {
+     return this.user != null &&
+       this.record && (this._isOwner() || this._isAdmin());
+   }
+ 
+   update() {
+     return this.edit();
+   }
 
-    new() {
-        return this.new();
-      }
-    
-    create() {
-        return this.new();
-      }
-    
-    edit() {
-        return this._isOwner() || this._isAdmin();
-      }
-    
-    update() {
-        return this.edit();
-      }
-    
-    destroy() {
-        return this.update();
-      }
-}
+   destroy() {
+     return this.update();
+   }
+ }
